41138fe734ec30956c4bad506838b7a959bb4d9e
fikri <me@fikri.tech>
2020-11-06 15:33:38 +0700

    add login page

---
+++ notes.js
@@ -0,0 +1,7 @@
+// ada dua cara 
+    // cara simple (projek kecil, developer dikit)
+    // cara ribet (projek gede, developer gede, contribusi open source)
+
+
+// cara simple
+    // push and pull
